398d988a4610a4d746900e8159520d439719d1f3
Ivan <86902525+JoStooop@users.noreply.github.com>
2022-02-17 04:02:00 +0300

    push

---
+++ src/js/calc-phone.js
@@ -0,0 +1,118 @@
+let numberOne = null
+let numberTwo = null
+let operation = null
+
+const BTN_OPERATION = {
+    OPERANDS: document.querySelectorAll('.btn-operation'),
+    CLEAR: document.querySelector('.buttons-phone__ac'),
+    ERASE: document.querySelector('.buttons-phone__erase'),
+    DOT: document.querySelector('.buttons-phone__dot'),
+};
+
+const UI_ELEMENTS = {
+    CALC_TOP_RESULT: document.querySelector('.screen-phone__top-result'),
+    CALC_BOTTOM_RESULT: document.querySelector('.screen-phone__bottom-result'),
+    CALC_BTNS: document.querySelectorAll('.btn-phone')
+};
+
+
+
+function checkFontSize() {
+    const screen = UI_ELEMENTS['CALC_BOTTOM_RESULT']
+    const lengthCheckOne = screen.textContent.length >= 6
+    const lengthCheckTwo = screen.textContent.length >= 8
+    // const lengthCheckThree = screen.textContent.length
+
+    if (lengthCheckOne) {
+        screen.style.fontSize = "70px"
+    }
+    if (lengthCheckTwo) {
+        screen.style.fontSize = "60px"
+    }
+    // if(screen.textContent.length === 10){
+         
+    // }
+}
+
+BTN_OPERATION['ERASE'].addEventListener('click', () => {
+    const screen = UI_ELEMENTS['CALC_BOTTOM_RESULT']
+
+    screen.textContent = screen.textContent.slice(0, screen.textContent.length - 1)
+})
+
+BTN_OPERATION['CLEAR'].addEventListener('click', () => {
+    UI_ELEMENTS['CALC_BOTTOM_RESULT'].textContent = '0'
+    numberTwo = ''
+    numberOne = ''
+});
+
+
+UI_ELEMENTS['CALC_BTNS'].forEach((element) => {
+    element.addEventListener('click', () => {
+        checkFontSize()
+
+        const screen = UI_ELEMENTS['CALC_BOTTOM_RESULT']
+
+        if (!numberOne) {
+            screen.textContent === '0' ? screen.textContent = element.textContent : screen.textContent += element.textContent
+        } else {
+            screen.textContent === numberOne ? numberTwo = (screen.textContent = element.textContent) : numberTwo = (screen.textContent += element.textContent)
+        }
+    })
+});
+
+
+BTN_OPERATION['OPERANDS'].forEach((element) => {
+    element.addEventListener('click', result)
+})
+
+function result() {
+    const screen = UI_ELEMENTS['CALC_BOTTOM_RESULT']
+
+    switch (this.textContent) {
+
+        case '+':
+            numberOne = screen.textContent
+            operation = 'plus'
+            break
+
+        case '-':
+            numberOne = screen.textContent
+            operation = 'minus'
+            break
+
+        case 'x':
+            numberOne = screen.textContent
+            operation = 'mult'
+            break
+
+        case '÷':
+            numberOne = screen.textContent
+            operation = 'div'
+            break
+
+        case '=':
+            screen.textContent = calc()
+            UI_ELEMENTS['CALC_TOP_RESULT'].textContent = screen.textContent
+            break
+
+        default:
+
+    }
+}
+function calc() {
+
+    if (operation === "div") {
+        return numberOne / numberTwo
+    }
+    if (operation === "mult") {
+        return numberOne * numberTwo
+    }
+    if (operation === "minus") {
+        return numberOne - numberTwo
+    }
+    if (operation === "plus") {
+        return +numberOne + +numberTwo
+    }
+}
+
