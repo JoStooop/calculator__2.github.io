1e2604522e02f1e7c1e0835cead0fcf145b9c6ec
Ivan <86902525+JoStooop@users.noreply.github.com>
2022-02-19 22:04:10 +0300

    push - проверка

--- js/calc-phone.js
+++ js/calc-phone.js
@@ -1,8 +1,20 @@
-import {BTN_OPERATION, UI_ELEMENTS} from './view.js'
-
 let numberOne = null
 let numberTwo = null
 let operation = null
+
+const BTN_OPERATION = {
+    OPERANDS: document.querySelectorAll('.btn-operation'),
+    CLEAR: document.querySelector('.buttons-phone__ac'),
+    ERASE: document.querySelector('.buttons-phone__erase'),
+    DOT: document.querySelector('.buttons-phone__dot'),
+};
+
+const UI_ELEMENTS = {
+    CALC_TOP_RESULT: document.querySelector('.screen-phone__top-result'),
+    CALC_BOTTOM_RESULT: document.querySelector('.screen-phone__bottom-result'),
+    CALC_BTNS: document.querySelectorAll('.btn-phone')
+};
+
 
 function checkFontSize() {
     const screen = UI_ELEMENTS['CALC_BOTTOM_RESULT']
